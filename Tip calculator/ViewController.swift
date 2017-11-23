2cb91ee04736a035aadb5b84358411c9c0a4b505
Anirudh V <anirudhv@Anirudhs-MacBook-Pro.local>
2017-11-23 11:31:42 -0800

    First IOS app

--- Tip calculator/ViewController.swift
+++ Tip calculator/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var amountt: UITextField!
+    
+    @IBOutlet weak var resultt: UILabel!
+    @IBOutlet weak var tipt: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func calc(_ sender: Any) {
+        var amount = Double(amountt.text!)
+        var tip = Double(tipt.text!)
+        var tipamount = amount! * tip!/100
+        var total = amount! + tipamount
+        resultt.text = "Total is $\(total)"
+    }
+    
 }
 
